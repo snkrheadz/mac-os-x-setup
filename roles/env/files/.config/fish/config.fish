8084474196cfe72e7e6e0fe0f208c2a42ffeebcb
akinrt <aki.d.sc@gmail.com>
2017-03-14 13:09:06 +0900

    update .config/fish/config.fish

--- roles/env/files/.config/fish/config.fish
+++ roles/env/files/.config/fish/config.fish
@@ -42,28 +42,28 @@ set -x HOMEBREW_GITHUB_API_TOKEN 8024d88a7af824245cf431bab016273230880e48
 
 # bobthefish options
 
-set -g theme_display_git yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_git_worktree_support yes
-set -g theme_display_vagrant yes
-set -g theme_display_docker_machine yes
-set -g theme_display_hg yes
+# set -g theme_display_git yes
+# set -g theme_display_git_untracked yes
+# set -g theme_display_git_ahead_verbose yes
+# set -g theme_git_worktree_support yes
+# set -g theme_display_vagrant yes
+# set -g theme_display_docker_machine yes
+# set -g theme_display_hg yes
 # set -g theme_display_virtualenv yes
 set -g theme_display_ruby yes
-set -g theme_display_user yes
-set -g theme_display_vi yes
-set -g theme_display_date yes
-set -g theme_display_cmd_duration yes
-set -g theme_title_display_process yes
-set -g theme_title_display_path yes
-set -g theme_title_use_abbreviated_path yes
-# set -g theme_date_format "+%a %H:%M"
+# set -g theme_display_user yes
+# set -g theme_display_vi yes
+# set -g theme_display_date yes
+# set -g theme_display_cmd_duration yes
+# set -g theme_title_display_process yes
+# set -g theme_title_display_path yes
+# set -g theme_title_use_abbreviated_path yes
+set -g theme_date_format "+%a %H:%M"
 # set -g theme_avoid_ambiguous_glyphs yes
-# set -g theme_powerline_fonts yes
+set -g theme_powerline_fonts no
 # set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
+# set -g theme_show_exit_status yes
 # set -g default_user your_normal_user
-set -g theme_color_scheme solarized-dark
+# set -g theme_color_scheme solarized-dark
 # set -g fish_prompt_pwd_dir_length 0
 # set -g theme_project_dir_length 1
